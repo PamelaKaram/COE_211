9390b65352393e5dffbd4e41393b41208b65a9b3
PamelaKaram <pamela.karam01@lau.edu>
2022-02-22 22:11:10 +0200

    lab 4

---
+++ StaffManagement/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+	public String firstName;
+	public String lastName;
+	public int age;
+	public double monthlySal;
+	
+
+	public Employee(String firstName,String lastName, int age, double monthlySal) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.monthlySal = monthlySal; 
+	
+	
+	}
+
+}
+
+		
---
+++ StaffManagement/EntryPoint.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EntryPoint {
+	
+	public static void main(String[] args){
+			
+	Scanner scan = new Scanner(System.in);
+	
+	
+	System.out.println("Please input employee's first name: ");
+	String firstName = scan.nextLine();
+	
+	System.out.println("Please input the employee's last name: ");
+	String lastName = scan.nextLine();
+
+	System.out.println("Please input the employee's age: ");
+	int age = scan.nextInt();
+
+	System.out.println("Please input the employee's monthly salary: ");
+	double monthlySal = scan.nextDouble();
+	
+		
+
+	Employee x = new Employee(firstName, lastName, age, monthlySal); 
+	System.out.println("Employee information: " + x.firstName+ " " +x.lastName + ", " + x.age + ", " + x.monthlySal);
+	
+	
+	}
+}
