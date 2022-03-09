6f9acd50c1a121dddd547a4daddfa77a6eefa95e
PamelaKaram <pamela.karam01@lau.edu>
2022-03-09 07:46:34 +0200

    step 1-lab6

---
+++ lab_6/Calculator.java
@@ -0,0 +1,63 @@
+import java.util.Scanner; 
+public class Calculator {
+	private int num1;
+	private int num2;
+    	private String operator;
+
+    // Constructor to initialize the calculator
+    public Calculator() {
+	
+	Scanner scanCalc = new Scanner(System.in);
+	
+	System.out.print("Input the first number: ");
+	num1 = scanCalc.nextInt();
+	
+	System.out.println("Input the operator: " );
+	operator = scanCalc.nextString();	
+	
+	System.out.println("Input the second number: ");
+	num2 = scanCalc.nextInt();
+
+   
+      switch(operator){
+		case "+":
+			System.out.println(add(num1,num2));
+			break;
+			
+		case "-":
+			System.out.println(subtract(num1,num2));
+			break;
+		
+		case "*": 
+			System.out.println(mutiply(num1,num2));
+			break;
+	
+		default:
+
+			System.out.println(divide(num1,num2));
+    }
+		
+		
+	
+    }
+
+    public String add(int a, int b) {
+       
+	  return num1 + num2;
+    }
+
+    public String subtract(int a, int b) {
+       
+	  return num1 - num2;
+    }
+
+    public String multiply(int a, int b) {
+       
+	  return num1 * num2;
+    }
+
+    public String divide(int a, int b) {
+      
+	  return num1 / num2;
+    }
+}
